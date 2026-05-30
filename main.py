0da64de3c4db247a427d699e1f9eae0019b71718
Germ-Slayer <ali.qamar.abbasi@gmail.com>
2026-05-30 11:27:43 +0500

    i commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from wrotools import *
 
 async def main():
     #await moveAttachmentArms(30, -270)
-    await attachment_left.reset_angle(0)
-    await attachment_right.reset_angle(0)
-    await moveAttachmentArms(30, 360+180)
+    #await attachment_left.run_target(-20,0)
+    #await attachment_right.run_target(-20,0)
+    await moveAttachmentArms(30, 360+160)
     await db.straight(200)
     await db.turn(90)
     await db.straight(275)
@@ -12,11 +12,21 @@ async def main():
     await db.straight(-300)
     await db.turn(90)
     await db.straight(1000)
-    await db.turn(90)
-    await db.straight(300)
     await db.turn(-90)
-    await db.straight(200)
-    await moveAttachmentArms(30, 270)
+    await db.straight(270)
+    await db.turn(90)
+    await db.straight(270)
+    await moveAttachmentArms(30, 160)
+    await db.straight(-270)
+    #await db.turn(-90)
+    #await db.straight(-270)
+    #await db.turn(90)
+    #await db.straight(270)
+    #await db.turn(-90)
+    
+
+
+    
 
 if __name__ == "__main__":
     run_task(main())
