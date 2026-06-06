57af7f43f4f7ae16a2da82acd939a243efd79bda
MeekooDude1998 <arsalanfaruqi@gmail.com>
2026-06-06 13:12:19 +0500

    ali is hottie and arsi is a chad but mikoo is a chud

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from wrotools import *
 
+
+
 async def main():
     #await moveAttachmentArms(30, -270)
-    await db.straight(90)
-    await db.turn(90)
-    await db.straight(330)
     await moveAttachmentArms(30, 450)
     await db.straight(190)
     await db.turn(90)
-    await db.straight(305)
+    await db.straight(310)
     
     #await db.straight(115)    
     await moveAttachmentArms(30, -390)
     await db.straight(-325)
     await db.turn(90)
-    await db.straight(250)
-    await db.turn(-90)
-    await db.straight(750)
+    await db.straight(1000)
     await db.turn(-90)
     await db.straight(270)
     await db.turn(90)
-    await db.straight(300)
-    await moveAttachmentArms(30, -360)
+    await db.straight(330)
+    await moveAttachmentArms(30, 240)
     await db.straight(-270)
     #await db.turn(-90)
     #await db.straight(-270)
@@ -34,6 +31,7 @@ async def main():
     
 
 if __name__ == "__main__":
+    db.distance_control.pid()
     run_task(main())
  
  
